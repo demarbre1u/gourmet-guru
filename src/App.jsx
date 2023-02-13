1ddadff3643e4f9ffa82b1355692f44975b29602
Allan Demarbre <ademarbre@groupe-cac.com>
2023-02-13 16:16:15 +0100

    Design

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,41 @@ import "./App.css";
 import dataJson from "./assets/data/data.json";
 import Card from "./components/Card";
 import { AiOutlineCloseCircle } from "react-icons/ai";
-import { useEffect, useState } from "react";
+import { RxReset } from "react-icons/rx";
+import { useEffect, useRef, useState } from "react";
+import Confetti from "react-confetti";
 
 function App() {
     const [data, setData] = useState(dataJson.data);
+
+    const firstCard = useRef();
+    const secondCard = useRef();
 
     useEffect(() => {
         setData(state => state.sort(() => Math.random() - 0.5));
     }, []);
 
+    const currentStep = dataJson.data.length - data.length + 1;
+    const totalStep = dataJson.data.length - 1;
+
     const removeItem = index => {
-        let tmpData = data.filter((value, i) => index !== i);
+        console.log({ firstCard: firstCard.current });
+
+        firstCard.current.classList.add("fadeLeft");
+        secondCard.current.classList.add("fadeRight");
+
+        setTimeout(() => {
+            firstCard.current.classList.remove("fadeLeft");
+            secondCard.current.classList.remove("fadeRight");
+
+            let tmpData = data.filter((value, i) => index !== i);
+            tmpData.sort(() => Math.random() - 0.5);
+            setData(tmpData);
+        }, 800);
+    };
+
+    const resetData = () => {
+        let tmpData = dataJson.data;
         tmpData.sort(() => Math.random() - 0.5);
         setData(tmpData);
     };
@@ -21,23 +45,35 @@ function App() {
         <div className="App">
             {data.length > 1 ? (
                 <>
-                    <div>
+                    <div className="card-wrapper" ref={firstCard}>
                         <Card data={data[0]} />
                         <button className="card__button" onClick={() => removeItem(0)}>
                             <AiOutlineCloseCircle fontSize="4em" />
                         </button>
                     </div>
-                    <div>
+                    <div className="card-wrapper" ref={secondCard}>
                         <Card data={data[1]} />
                         <button className="card__button" onClick={() => removeItem(1)}>
                             <AiOutlineCloseCircle fontSize="4em" />
                         </button>
                     </div>
+
+                    <div className="step-count">
+                        Étape {currentStep} sur {totalStep}
+                    </div>
                 </>
             ) : (
                 <div>
+                    <Confetti width={window.innerWidth} height={window.innerHeight} />
                     <span className="winner">Le gagnant est :</span>
-                    <Card data={data[0]} />
+
+                    <div className="card-winner">
+                        <Card data={data[0]} />
+                    </div>
+
+                    <button className="reset-button" onClick={resetData}>
+                        <RxReset color="#242424" fontSize="3em" />
+                    </button>
                 </div>
             )}
         </div>
